544257e2547108717994e9313f2deefe0ba0dd90
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-02-20 20:39:31 -0500

    Version increment.

--- Plugin.cs
+++ Plugin.cs
@@ -11,7 +11,7 @@ using BepInEx.Logging;
 using System.Linq;
 
 namespace BossNotifier {
-    [BepInPlugin("Mattdokn.BossNotifier", "BossNotifier", "1.2.1")]
+    [BepInPlugin("Mattdokn.BossNotifier", "BossNotifier", "1.3.1")]
     public class BossNotifierPlugin : BaseUnityPlugin {
         public static ConfigEntry<KeyboardShortcut> showBossesKeyCode;
         public static ConfigEntry<bool> showNotificationsOnRaidStart;
