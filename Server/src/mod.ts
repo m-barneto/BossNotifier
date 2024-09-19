58283f1bb07cd9594ec173cf463ee203480c6497
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-09-18 20:24:36 -0400

    Added reference to spt-common.

--- Server/src/mod.ts
+++ Server/src/mod.ts
@@ -1,25 +1,43 @@
-import { DependencyContainer } from "tsyringe";
+import { DependencyContainer, inject, injectable } from "tsyringe";
 import { IPreSptLoadMod } from "@spt/models/external/IPreSptLoadMod";
 import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { jsonc } from "jsonc";
 import { VFS } from "@spt/utils/VFS";
 import path from "path";
 import { PreSptModLoader } from "@spt/loaders/PreSptModLoader";
+import { StaticRouterModService } from "@spt/services/mod/staticRouter/StaticRouterModService";
 
+@injectable()
 class Mod implements IPreSptLoadMod {
     private config;
+    private fikaMatchService;
     preSptLoad(container: DependencyContainer): void {
         const logger = container.resolve<ILogger>("WinstonLogger");
         const vfs = container.resolve<VFS>("VFS");
         this.config = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config/config.jsonc")));
         
         const modImporter = container.resolve<PreSptModLoader>("PreSptModLoader");
-        
-        logger.info("Mods in order:");
-        const modList = modImporter.sortModsLoadOrder();
-        for (const mod in modList) {
-            logger.info(modList[mod]);
-        }
+        const hasFika = modImporter.getImportedModsNames().includes("fika-server");
+        //if (!hasFika) return;
+
+        const staticRouterModService = container.resolve<StaticRouterModService>("StaticRouterModService");
+
+        staticRouterModService.registerStaticRouter(
+            "BossNotifierRouter",
+            [
+                {
+                    url: "/bosses/",
+                    action: async (url, info, sessionId, output) => {
+                        logger.info(url);
+                        logger.info(info);
+                        logger.info(sessionId);
+                        logger.info(output);
+                        return JSON.stringify({ bosses: [ "urmom", "cthulu", "jeff" ]});
+                    }
+                }
+            ],
+            "bossnotifier"
+        )
     }
 }
 
